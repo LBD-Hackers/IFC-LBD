d9ed59ae0d28dd7dff25efa86fe29db781a2ad02
Mads Holten Rasmussen <mads@madss-air.lan>
2022-01-23 15:29:58 +0100

    Added repo to package.json

--- lib/types/src/helpers/communica-extension-functions.d.ts
+++ lib/types/src/helpers/communica-extension-functions.d.ts
@@ -4,8 +4,6 @@ export declare const extensionFunctions: {
     'http://example.org/functions#get-id'(args: RDF.Term[]): RDF.BaseQuad | RDF.BlankNode | RDF.Variable | RDF.DefaultGraph | import("rdf-data-factory").Literal;
     'http://example.org/functions#get-namespace'(args: RDF.Term[]): RDF.BaseQuad | RDF.BlankNode | RDF.Variable | RDF.DefaultGraph | import("rdf-data-factory").Literal;
     'http://example.org/functions#uri-concat'(args: RDF.Term[]): import("rdf-data-factory").Literal | import("rdf-data-factory").NamedNode<string>;
-};
-export declare const geoSPARQLFunctions: {
     'http://www.opengis.net/def/function/geosparql/distance'(args: RDF.Term[]): import("rdf-data-factory").Literal;
 };
 //# sourceMappingURL=communica-extension-functions.d.ts.map
--- lib/types/src/helpers/wkt-parser.d.ts
+++ lib/types/src/helpers/wkt-parser.d.ts
@@ -1,2 +1,2 @@
-export declare function parseWKT(wktString: string): void;
+export declare function parseWKT(wktString: string): number[];
 //# sourceMappingURL=wkt-parser.d.ts.map
