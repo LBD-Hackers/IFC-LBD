01abe1c34e70aa6c4aa4f48c34178f099f2e0d1d
Mads Holten Rasmussen (MHRA) <mhra@NIRAS.DK>
2023-01-16 10:07:51 +0100

    Proper export of helpers

--- src/cli-tool.ts
+++ src/cli-tool.ts
@@ -17,7 +17,7 @@ export class CLITool{
     public async getArgs(): Promise<any>{
 
         return await yargs(hideBin(process.argv))
-            .version("0.3.1")
+            .version("0.3.4")
             .command('[subset]', 'parse ifc', (yargs) => {
                 return yargs
                 .positional('subset', {
